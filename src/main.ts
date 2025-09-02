c62c99d08d2b98369788cccd3803f7ed1e500790
Joseph Afriyie Attakorh <juxluvjoe@gmail.com>
2025-09-02 18:27:38 +0000

    fix: add cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AllExceptionsFilter } from './common/all-exceptions/all-exceptions.filt
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
   const config = new DocumentBuilder()
